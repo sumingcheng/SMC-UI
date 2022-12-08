585fa6e7fb87a8562610eba6e448004c2260089c
Sumingcheng <fanxiaohaohaha@qq..com>
2022-12-08 11:09:27 +0800

    2022年12月8日 11:09:24

--- src/main.js
+++ src/main.js
@@ -9,18 +9,8 @@ import '@/assets/index.scss';
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// 引入全局工具类
-import * as utils from '@/tools/Tool';
-// 注册过滤器
-import * as filters from '@/filters';
 
 Vue.use(ElementUI);
-
-Vue.prototype.$utils = utils;
-
-Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key]);
-});
 
 new Vue({
   router,
