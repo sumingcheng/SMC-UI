23a1430a5eb1d8ab2e65e28c18679b6c0106b5dd
Sumingcheng <fanxiaohaohaha@qq..com>
2022-12-08 12:03:26 +0800

    2022年12月8日 12:03:22

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,23 +4,118 @@
         background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
       <el-menu-item index="1">树形组件</el-menu-item>
       <el-submenu index="2">
-        <template slot="title">分页组件</template>
+        <template v-slot="title">分页组件</template>
         <el-menu-item index="2-1">太原封装</el-menu-item>
       </el-submenu>
     </el-menu>
     <div class="content">
-      <h1>内容部分</h1>
+      <treeComponent :TreeData="TreeData"></treeComponent>
     </div>
   </div>
 </template>
 
 <script>
+import treeComponent from '@/views/treeComponent/index.vue';
+
 export default {
   data() {
     return {
       activeIndex: '1',
-      activeIndex2: '1'
+      activeIndex2: '1',
+      TreeData: [ {
+        id: 1,
+        label: '一级 1',
+        children: [ {
+          id: 4,
+          label: '二级 1-1',
+          children: [ {
+            id: 9,
+            label: '三级 1-1-1'
+          }, {
+            id: 10,
+            label: '三级 1-1-2'
+          } ]
+        } ]
+      },
+        {
+          id: 2,
+          label: '一级 2',
+          children: [ {
+            id: 5,
+            label: '二级 2-1'
+          }, {
+            id: 6,
+            label: '二级 2-2'
+          } ]
+        },
+        {
+          id: 3,
+          label: '一级 3',
+          children: [ {
+            id: 7,
+            label: '二级 3-1'
+          }, {
+            id: 8,
+            label: '二级 3-2',
+            children: [ {
+              id: 11,
+              label: '三级 3-2-1'
+            }, {
+              id: 12,
+              label: '三级 3-2-2'
+            }, {
+              id: 13,
+              label: '三级 3-2-3'
+            } ]
+          } ]
+        },
+        {
+          id: 4,
+          label: '一级 4',
+          children: [ {
+            id: 7,
+            label: '二级 3-1'
+          }, {
+            id: 8,
+            label: '二级 3-2',
+            children: [ {
+              id: 11,
+              label: '三级 3-2-1'
+            }, {
+              id: 12,
+              label: '三级 3-2-2'
+            }, {
+              id: 13,
+              label: '三级 3-2-3'
+            } ]
+          } ]
+        },
+        {
+          id: 5,
+          label: '一级 5',
+          children: [ {
+            id: 7,
+            label: '二级 3-1'
+          }, {
+            id: 8,
+            label: '二级 3-2',
+            children: [ {
+              id: 11,
+              label: '三级 3-2-1'
+            }, {
+              id: 12,
+              label: '三级 3-2-2'
+            }, {
+              id: 13,
+              label: '三级 3-2-3'
+            } ]
+          } ]
+        }
+      ],
     };
+  },
+  components: {
+    treeComponent
   },
   methods: {
     handleSelect(key, keyPath) {
--- src/views/treeComponent/index.vue
+++ src/views/treeComponent/index.vue
@@ -1,11 +1,52 @@
 <template>
   <div>
-
+    <el-tree class="treeStyle" :data="TreeData" :props="defaultProps" :default-expanded-keys="expandArray"
+        :filter-node-method="filterNode" @node-click="treeClick" ref="tree" :expand-on-click-node="true"
+        node-key="id" :indent="30">
+    </el-tree>
   </div>
 </template>
-
 <script>
-export default {};
+
+export default {
+  props: {
+    TreeData: {
+      type: Array,
+      default: () => []
+    },
+  },
+  data() {
+    return {
+      expandArray: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+    treeClick(data) {
+      this.temp.push(data.id);
+      this.treeClickData = data;
+      let t = this.temp.some((elem, index, arr) => {
+        if (elem === arr[index - 1]) {
+          this.temp = [];
+          this.submitTree();
+          return true;
+        } else {
+          return false;
+        }
+      });
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.unitName.indexOf(value) !== - 1;
+    }
+  }
+};
 </script>
 
 <style scoped>
